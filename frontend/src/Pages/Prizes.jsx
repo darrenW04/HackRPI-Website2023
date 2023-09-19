74293564e63de6390e27645a37bffe95e5546ae2
darrenW04 <darrenwong1000@gmail.com>
2023-09-19 16:51:18 -0400

    first commit

---
+++ frontend/src/Pages/Prizes.jsx
@@ -0,0 +1,9 @@
+const Prizes = () => {
+  return (
+    <div>
+      <img src="https://media.tenor.com/UlHfWEAw05QAAAAM/tayomaki-sakigifs.gif"></img>
+    </div>
+  );
+};
+
+export default Prizes;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,39 +8,43 @@ import AboutPage from './Pages/AboutPage/AboutPage.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SponsorPage from './Pages/SponsorPage/SponsorPage.jsx';
+import Prizes from './Pages/Prizes.jsx';
+
 import './style.css';
 // import './fonts.css';
 
 function App() {
-	const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setIsMobile(window.innerWidth <= 900);
-		};
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 900);
+    };
 
-		handleResize(); // Check initial screen size
+    handleResize(); // Check initial screen size
 
-		window.addEventListener('resize', handleResize);
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-	return (
-		<div>
-			<NavBar />
-			{/* <LandingPage /> */}
-			{/* currently swapped for testming, swap back before commiting */}
-			{isMobile ? <LandingPageMobile /> : <LandingPage />}
-			{/* re-add if timer is fixed for both mobile and web */}
-			{/* <Timer /> */}
-			<AboutPage />
-			<FAQPage />
-			<SponsorPage/>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <NavBar />
+      {/* <LandingPage /> */}
+      {/* currently swapped for testming, swap back before commiting */}
+      {isMobile ? <LandingPageMobile /> : <LandingPage />}
+      {/* re-add if timer is fixed for both mobile and web */}
+      {/* <Timer /> */}
+      <Prizes />
+
+      <AboutPage />
+      <FAQPage />
+      <SponsorPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
